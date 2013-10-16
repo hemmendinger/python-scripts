22c38b6791d635bf5f6b4716601e39b57ccbf56c
Hemmendinger <github@hemmendinger.net>
2013-10-16 11:29:52 -0400

    short script to monitor a specified file and notify via sound when gone

---
+++ file_existence_monitor.py
@@ -0,0 +1,58 @@
+import os, sys, pygame
+from time import sleep
+
+'''
+Notify if files in a directory have changed, but only by file names.
+
+Usage:
+python script.py FILENAME_TO_WATCH INTEGER
+
+Use purpose:
+Want to be notified when a file finishes downloading in Chrome
+ithout specifying a file. Chrome creates a temporary download file
+and renames it when the download completes.
+
+'''
+
+file = sys.argv[1]
+soundplay = int(sys.argv[2])
+cwd = os.getcwd()
+soundfile = "/usr/share/sounds/KDE-K3B-Finish-Success.ogg"
+
+
+def file_check(file):
+    if file in os.listdir(cwd):
+        return True
+    else:
+        return False
+
+def play_sound(soundfile, soundplay):
+    sound = []
+    if soundplay:
+        pygame.mixer.init()
+        for x in range(soundplay):
+            sound = pygame.mixer.Sound(soundfile)
+            sound.play()
+            while pygame.mixer.get_busy():
+                sleep(1)
+
+def initial_check(file):
+    if file_check(file) == False:
+        print("Terminating: File not found.")
+        sys.exit()
+    else:
+        exists = True
+
+def monitor(file):
+    while True:
+        if file_check(file):
+            sleep(5)
+        else:
+            play_sound(soundfile, soundplay)
+            print("Temporary download file no longer found: " + file)
+            sys.exit()
+
+if __name__ == '__main__':
+#    play_sound(soundfile, soundplay)
+    initial_check(file)
+    monitor(file)
