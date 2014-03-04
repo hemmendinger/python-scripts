5dae79cdbd7ac06ec3f9b1e025987a1dc74cc693
Hemmendinger <github@hemmendinger.net>
2014-03-04 17:45:14 -0500

    added --onlyvisible, and renamed argument for activate_window, kept activate_window loop because more than one instance might be available to activate

--- activate_window.py
+++ activate_window.py
@@ -3,16 +3,16 @@ import subprocess
 
 def get_process(name):
     """Search for the windows containing name and return list of IDs"""
-    window_ids = subprocess.check_output(["xdotool", "search", "--name", name])
+    window_ids = subprocess.check_output(["xdotool", "search", "--onlyvisible", "--name", name])
     window_ids = window_ids.splitlines() # split str by linebreaks
     return window_ids
 
-def activate_window(window_id):
+def activate_window(window_ids):
     """Iterates the identifiers, trying to activate each one"""
-    for identifier in window_id:
-        subprocess.call(["xdotool", "windowactivate", identifier ])
+    for identifier in window_ids:
+        subprocess.call(["xdotool", "windowactivate", identifier])
 
 
 if __name__ == '__main__':
-    print get_process("emacs")
+     get_process("emacs")
 
