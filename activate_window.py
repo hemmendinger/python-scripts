19502554b4cfe369eff1ef3a514128a0422f94ab
Hemmendinger <github@hemmendinger.net>
2014-03-04 17:39:24 -0500

    added docstring, found option to only get visible windows, maybe dont need to loop ids

--- activate_window.py
+++ activate_window.py
@@ -1,15 +1,17 @@
 import os
 import subprocess
 
-def get_process(process):
-    pid = subprocess.check_output(["xdotool", "search", "--name", process])
-    pid = pid.splitlines() # split str by linebreaks
+def get_process(name):
+    """Search for the windows containing name and return list of IDs"""
+    window_ids = subprocess.check_output(["xdotool", "search", "--name", name])
+    window_ids = window_ids.splitlines() # split str by linebreaks
+    return window_ids
 
+def activate_window(window_id):
+    for identifier in window_id:
+        subprocess.call(["xdotool", "windowactivate", identifier ])
 
-def test(process):
-    pid = call(["pgrep", process])
-    print pid
 
 if __name__ == '__main__':
-    get_process("emacs")
+    print get_process("emacs")
 
