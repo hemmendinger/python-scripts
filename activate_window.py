f33bfc3afb7e5ccbbd1e1af907bfc098a789eddc
Hemmendinger <github@hemmendinger.net>
2014-03-05 14:37:14 -0500

    activate_window.py docs updated

--- activate_window.py
+++ activate_window.py
@@ -1,9 +1,20 @@
 """
-Activate the window of a program.
+Activate the window of a program under Ubuntu/Linux.
+
+Dependency:
+    xdotools
+    Python (Tested using Python 2.7.3 and Python 3.2.3)
 
 Usage:
     python activate_window.py NAME_OF_PROGRAM
 
+Purpose:
+Alt-Tab can be an unnecessary strain on the wrists during extended
+coding sessions.
+
+If you find yourself needing to use Alt-Tab with a bunch of windows
+open, and you usually just want to switch back to a text editor or
+browser, assign this script to a shortcut.
 """
 
 from sys import argv
